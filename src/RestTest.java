39f425b4f00ee0f14b296f4ffe2420e51bb2e9a0
WileyEdServices <WES-SRPGRPDEV@deltak-innovation.com>
2017-05-04 13:59:14 -0500

    SFDC LOGIN AND REST CALL EXAMPLE

--- src/RestTest.java
+++ src/RestTest.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-
+import java.util.Properties;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -22,77 +20,52 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-
 /**
  * This program demonstrates the following basic use cases for the REST API: -
  * authentication with OAuth 2.0 (This is for example development purposes only.
- * Not a real implementation.) - querying (using account records) - inserting
- * (using a contact record related to one of the retrieved account records) -
- * updating (updates contact record added in previous step)
+ * Not a real implementation.)
  * 
- * @author salesforce training
+ * @author Wiley Ed Services
  */
 public class RestTest extends Object {
-
-	
 	// ---------Credentials----------
 	// Credentials providing access to a specific Salesforce organization.
-	private static final String userName = readFile("username.txt"); // COPY
-																						// USERNAME
-	private static final String password = readFile("password.txt"); // COPY
-																					// PASSWORD
-																					// AND
-																					// TOKEN
-
+	private static final String userName = getProperty("username"); // Get Username from Properties
+	private static final String password = getProperty("password"); // Get Password from Properties
 	// ---------REST and OAuth-------
 	// Portions of the URI for REST access that are re-used throughout the code
-	private static String OAUTH_ENDPOINT = "/services/oauth2/token";
-	private static String REST_ENDPOINT = "/services/apexrest/";
-
+	private static String OAUTH_ENDPOINT = getProperty("OAUTH_ENDPOINT");
+	private static String REST_ENDPOINT = getProperty("REST_ENDPOINT");
 	// Holds URI returned from OAuth call, which is then used throughout the
 	// code.
 	String baseUri;
-
 	// The oauthHeader set in the oauth2Login method, and then added to
 	// each HTTP object that is used to invoke the REST API.
 	Header oauthHeader;
-
 	// Basic header information added to each HTTP object that is used
 	// to invoke the REST API.
 	Header prettyPrintHeader = new BasicHeader("X-PrettyPrint", "1");
-
 	// ----------Utility-------------
 	// Used to get input from console.
 	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
 	// ================Code starts here===================
 	public static void main(String[] args) throws ParseException {
 		new RestTest();
 	}
-
 	/**
 	 * This class holds all the values related to the credentials needed to make
-	 * the OAuth2 request for authentication. Normally they would not be set in
-	 * this manner.
+	 * the OAuth2 request for authentication.
 	 */
 	class UserCredentials {
-		String loginInstanceDomain = "test.salesforce.com"; // COPY YOUR SERVER
-															// INSTANCE
-		String apiVersion = "37"; // COPY YOU API VERSION
+		String loginInstanceDomain = getProperty("loginInstanceDomain"); // USE YOUR SERVER INSTANCE
+		String apiVersion = "37"; // USE YOUR API VERSION
 		String userName = RestTest.userName;
 		String password = RestTest.password;
-		String consumerKey = readFile("consumer_key.txt"); // COPY
-																														// YOUR
-																														// CONSUMER
-																														// //
-																														// KEY
-		String consumerSecret = readFile("consumer_secret.txt"); // COPY YOUR CONSUMER
-														// SECRET
+		String consumerKey = getProperty("consumer_key"); // USE YOUR CONSUMER KEY																												// KEY
+		String consumerSecret = getProperty("consumer_secret"); //USE YOUR CONSUMER SECRET
 		String grantType = "password";
-		String redirect = "https://www.google.com";
+		String redirect = getProperty("redirect");
 	}
-
 	/**
 	 * Constructor drives console interaction and calls appropriate methods.
 	 * 
@@ -135,22 +108,19 @@ public class RestTest extends Object {
 					this.oauth2Login();
 					loginCount++;
 				}
-
 				if (executionOption == 2) {
 					this.restPostExample();
 				}
-
 			}
 			System.out.println("Program complete.");
 		}
 	}
-
 	/**
 	 * This method connects the program to the Salesforce organization using
 	 * OAuth. It stores returned values for further access to organization.
 	 * 
 	 * @param userCredentials
-	 *            Contains all credentials necessary for login
+	 * Contains all credentials necessary for login
 	 * @return
 	 * @throws ParseException
 	 */
@@ -160,7 +130,6 @@ public class RestTest extends Object {
 		HttpResponse response = null;
 		UserCredentials userCredentials = new UserCredentials();
 		String loginHostUri = "https://" + userCredentials.loginInstanceDomain + OAUTH_ENDPOINT;
-
 		try {
 			// Construct the objects for making the request
 			HttpClient httpClient = new DefaultHttpClient();
@@ -179,10 +148,8 @@ public class RestTest extends Object {
 			requestBody.setContentType("application/x-www-form-urlencoded");
 			httpPost.setEntity(requestBody);
 			httpPost.addHeader(prettyPrintHeader);
-
 			// Make the request and store the result
 			response = httpClient.execute(httpPost);
-
 			// Parse the result if we were able to connect.
 			if (response.getStatusLine().getStatusCode() == 200) {
 				String response_string = EntityUtils.toString(response.getEntity());
@@ -210,23 +177,31 @@ public class RestTest extends Object {
 		}
 		return response;
 	}
-
 	/**
 	 * This method demonstrates - How to use HTTPPost and a constructed URI to
-	 * retrieve data into SRP. - Simple creation of a JSON object.
+	 * retrieve data from SFDC. - Simple creation of a JSON object.
 	 */
 	public void restPostExample() {
-		System.out.println("\n_______________ SRPGlobalApplicantOutAPI _______________");
-		String uri = baseUri + readFile("endpoint.txt");
+		System.out.println("\n_______________ OAuthForSFDCAccess _______________");
+		String uri = baseUri + getProperty("endpoint");
 		System.out.println("URI Value :  " + uri+"\n\n");
 		try {
 			// create the JSON object containing the new contact details.
+			/*
+			 * This JSON will be provided after a partner has been on-boarded
+			 * 		{
+			 *		"Unique_Identifier": "Unique_Identifier_Value",
+			 *		"Parameter 1": [
+			 *		"Value 1",
+			 *		"Value 2"
+			 *		],
+			 *		"Authentication_Key": "Authentication_Key_Value"
+			 *		}
+			 */
 			JSONParser parser = new JSONParser();
 			Object obj = parser.parse(new FileReader("post.json"));
-
 			JSONObject applicant = (JSONObject) obj;
 			System.out.println("JSON for retriving record between dates:\n" + applicant.toString()+"\n\n");
-
 			// Construct the objects needed for the request
 			DefaultHttpClient httpClient = new DefaultHttpClient();
 			HttpPost httpPost = new HttpPost(uri);
@@ -236,21 +211,17 @@ public class RestTest extends Object {
 			StringEntity body = new StringEntity(applicant.toString());
 			body.setContentType("application/json");
 			httpPost.setEntity(body);
-
 			// Make the request
 			HttpResponse response = httpClient.execute(httpPost);
-
 			// Process the results
 			int statusCode = response.getStatusLine().getStatusCode();
 			if (statusCode == 201 || statusCode == 200) {
 				String response_string = EntityUtils.toString(response.getEntity());
-
 				JSONParser parser1 = new JSONParser();
 				JSONObject json = (JSONObject) parser1.parse(response_string);
 				System.out.println("Retrive Successful! Status code returned is " + statusCode+"\n\n");
-				System.out.println("Response from SRP: " + json.toJSONString()+"\n\n");
+				System.out.println("Response from SFDC: " + json.toJSONString()+"\n\n");
 				System.out.println("httpPOST: " + httpPost+"\n\n");
-
 				// Store the retrieved data as follows.
 				// eg.....
 				// contactId = json.getString("id");
@@ -268,7 +239,6 @@ public class RestTest extends Object {
 			e.printStackTrace();
 		}
 	}
-
 	/**
 	 * Extend the Apache HttpPost method to implement an HttpPost method.
 	 */
@@ -276,12 +246,10 @@ public class RestTest extends Object {
 		public HttpPatch(String uri) {
 			super(uri);
 		}
-
 		public String getMethod() {
 			return "PATCH";
 		}
 	}
-
 	/**
 	 * This class is used to hold values returned by the OAuth request.
 	 */
@@ -291,27 +259,22 @@ public class RestTest extends Object {
 		String instance_url;
 		String signature;
 		String access_token;
-
 		public OAuth2Response() {
 		}
-
 		public OAuth2Response(JSONObject json) {
-
 			id = (String) json.get("id");
 			issued_at = (String) json.get("issued_at");
 			instance_url = (String) json.get("instance_url");
 			signature = (String) json.get("signature");
 			access_token = (String) json.get("access_token");
-
 		}
 	}
-
 	// ==========utility methods=============
 	/**
 	 * Utility method for changing a stream into a String.
 	 * 
 	 * @param inputStream
-	 * @return
+	 * @return String
 	 */
 	private String getBody(InputStream inputStream) {
 		String result = "";
@@ -328,10 +291,12 @@ public class RestTest extends Object {
 		}
 		return result;
 	}
-
 	// --------------utility methods for user input----------
 	/**
 	 * A utility method to be used for getting user input from the console.
+	 * 
+	 * @param prompt
+	 * @return String
 	 */
 	private String getUserInput(String prompt) {
 		String result = "";
@@ -343,7 +308,6 @@ public class RestTest extends Object {
 		}
 		return result;
 	}
-
 	/**
 	 * Outputs menu choices on console.
 	 */
@@ -354,36 +318,31 @@ public class RestTest extends Object {
 		System.out.println("99. EXIT");
 		System.out.println("   ");
 	}
-
-	// ===========READ END POINT========
-
-	public static String readFile(String filename) {
-		FileInputStream in = null;
+	/**
+	 * Get Properties from login_config.properties.
+	 * 
+	 * @param propertyName
+	 * @return String
+	 */
+	public static String getProperty(String propertyName) {
+		Properties prop = new Properties();
+		InputStream input = null;
 		try {
-			in = new FileInputStream(filename);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		String strLine, strString = "";
-		try {
-
-			while ((strLine = br.readLine()) != null) {
-				strString += strLine;
+			input = new FileInputStream("login_config.properties");
+			// load a properties file
+			prop.load(input);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-
-		try {
-			in.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return strString;
+		// get the property value and return it
+		return prop.getProperty(propertyName);
 	}
 }
